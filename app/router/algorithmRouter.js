ba918d9cff24c54e2af8d32c16d79743da3de2e9
EtiamSani <etiam.sani@laposte.net>
2023-04-20 19:25:20 +0200

    Merge & Picture + documentation

--- app/router/algorithmRouter.js
+++ app/router/algorithmRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const algorithmController = require("../controller/algorithmController");
 const router = express.Router();
 
+// Route de l'algo -> donne tout les information des psy dans une spécialité.
 router.get('/patient/:id',algorithmController.getTherapistBySurvey);
 
 module.exports = router;

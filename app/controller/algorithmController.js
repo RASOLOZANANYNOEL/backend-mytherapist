7815f87c549819b982f72ccf030f9cff7dd0b11f
EtiamSani <etiam.sani@laposte.net>
2023-04-26 14:01:25 +0200

    remove console logs

--- app/controller/algorithmController.js
+++ app/controller/algorithmController.js
@@ -13,7 +13,7 @@ const algorithmController = {
         async function therapist(id) {
 
             const getSurveyAnswer = await patientsDatamapper.getSurveyAnswer(id);
-            console.log(getSurveyAnswer)
+            
             let result = '';
 
             for (const answer of getSurveyAnswer) {
@@ -30,7 +30,7 @@ const algorithmController = {
                  */
                 if (!answer.answer_1 || answer.answer_3 === 'professionnal') {
                     getWorkTherapistInCompany = await therapistsDatamapper.findAllTherapistBySpecialties(1)
-                    console.log(getWorkTherapistInCompany)
+                    
                     return res.json(getWorkTherapistInCompany);
                 } else {
 
@@ -39,7 +39,6 @@ const algorithmController = {
                      * Ici on gère le cas ou la personne concernée est un enfant.
                      */
                     if (answer.answer_2 === 'child') {
-                        console.log(`Voici la liste des psychologues pour enfant ${gender} qui sauront vous aider`); //fetch
                         getChildrenTherapist = await therapistsDatamapper.findAllTherapistBySpecialtiesAndGender(7, gender)
                         result = getChildrenTherapist
                         return res.json(result);

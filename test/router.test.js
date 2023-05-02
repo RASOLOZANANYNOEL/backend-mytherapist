4c16775f63767f1a53a496e54a1400aa8ae74bda
EtiamSani <etiam.sani@laposte.net>
2023-05-02 21:02:02 +0200

    UT

--- test/router.test.js
+++ test/router.test.js
@@ -16,70 +16,72 @@ const app = require('../app.js');
   });
 }); */
 
-describe('GET /therapists', function() {
-  it('responds with json', async function(done) {
-    request(app)
-      .get('/therapists')
-      .expect('Content-Type', /json/)
-      .expect(200);
-      done();
-  });
-});
-    
+// describe('GET /therapists', function () {
+//   it('responds with json', async function (done) {
+//     request(app)
+//       .get('/therapists')
+//       .expect('Content-Type', /json/)
+//       .expect(200);
+//     done();
+//   });
+// });
 
-    describe('POST /therapists', function() {
-      it('responds with json', function(done) {
-        request(app)
-          .post('/therapists')
-          
-          .send({"firstname": "john",
-            "lastname": "fafa",
-            "password": "aaaddadaf",
-            "adelinumber": "147896526",
-            "streetname" : "zobzob zob",
-            "zipcode" : "56100",
-            "email" : "rerefaz@hotmail.com",
-            "city" : "panam",
-            "gender" : "Homme",
-            "role": "therapist"
-           })
-          
-          .expect(200)
-          .end(function(err, res) {
-            console.log(err)
-            if (err) return done(err);
-            return done();
-          });
-      });
-    //   // it ('should handle no message body', function(done) {
-    //   //       request(app)
-    //   //       .post('/therapists')
-    //   //       .set('Content-Type', 'application/json')
-    //   //       .end(function(err, res) {
-    //   //             res.should.have.status(200)
-    //   //             });
 
+describe('POST /therapists', function () {
+  it('responds with json', function (done) {
+    request("http://localhost:3000")
+      .post('/therapists')
+
+      .send({
+        "firstname": "john",
+        "lastname": "fafa",
+        "phonenumber": "0606060608",
+        "password": "aaaddadaf",
+        "adelinumber": "147896526",
+        "streetname": "zobzob zob",
+        "zipcode": "56100",
+        "email": "rerefaz@hotmail.com",
+        "city": "panam",
+        "gender": "Homme",
+        "role": "therapist"
       })
-   
+
+      .expect(201)
+      .end(function (err, res) {
+        console.log(err)
+        if (err) return done(err);
+        return done();
+      });
+  });
+  //   // it ('should handle no message body', function(done) {
+  //   //       request(app)
+  //   //       .post('/therapists')
+  //   //       .set('Content-Type', 'application/json')
+  //   //       .end(function(err, res) {
+  //   //             res.should.have.status(200)
+  //   //             });
+
+});
 
 
-  //   describe('GET /therapists/id', function() {
-  //     it('responds with json', function(done) {
-  //       request(app)
-  //         .get('/therapists')
-  //         .expect('Content-Type', /json/)
-  //         .expect(200, done)
-  //         return done()
-  //     });
-  //   });
+
+//   describe('GET /therapists/id', function() {
+//     it('responds with json', function(done) {
+//       request(app)
+//         .get('/therapists')
+//         .expect('Content-Type', /json/)
+//         .expect(200, done)
+//         return done()
+//     });
+//   });
 
 
-  // describe('GET /patients', function() {
-  //   it('responds with json', function(done) {
-  //     request(app)
-  //       .get('/patients')
-  //       .expect('Content-Type', /json/)
-  //       .expect(200, done)
-  //       return done()
-  //   });
-  // });
+// describe('GET /patients', function() {
+//   it('responds with json', function(done) {
+//     request(app)
+//       .get('/patients')
+//       .expect('Content-Type', /json/)
+//       .expect(200, done)
+//       return done()
+//   });
+// });
